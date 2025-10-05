b3824899248beafd03569d0e2ab32367cde085bb
efficacy38 <efficacy38@gmail.com>
2025-10-06 00:53:24 +0800

    feat: add notes entries for internal notes
    otherwise it would be refined post

--- src/config.ts
+++ src/config.ts
@@ -18,6 +18,10 @@ export const navBarConfig: NavBarConfig = {
             url: "/tags",
         },
         {
+            name: "Notes",
+            url: "/notes",
+        },
+        {
             name: "Projects",
             url: "/projects",
         },
